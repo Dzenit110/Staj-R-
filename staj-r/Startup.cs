7f6f24d7f2a565ac89cc86072141df7c56421494
AlpcnDlk <alpcndlk01@gmail.com>
2022-10-06 14:51:21 +0300

    ant design of blazor installation

--- staj-r/Startup.cs
+++ staj-r/Startup.cs
@@ -33,6 +33,7 @@ namespace staj_r
             ///////// COOKIE ///////////////
             services.AddBlazoredLocalStorage();
             //////////////// STOP ////////////
+            services.AddAntDesign();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
